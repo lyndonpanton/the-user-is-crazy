808f5d8002d1df2930de9cbfefd0ba31f35dacec
Lyndon Panton <LyndonPanton@gmail.com>
2023-02-18 23:03:21 +0000

    Completed problem 2
    Added catch blocks to handle possible exceptions

--- Exercise1/Exercise1/Program.cs
+++ Exercise1/Exercise1/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            int input = Convert.ToInt32(Console.ReadLine());
-        }
+
+            try
+            {
+
+                Console.Write("Enter a score (integer): ");
+                int input = Convert.ToInt32(Console.ReadLine());
+            } catch (FormatException exception)
+            {
+                Console.WriteLine("Error: Invalid data type supplied");
+            } catch (OverflowException exception)
+            {
+                Console.WriteLine("Error: Input supplied is outside the valid range");
+            }
+        } 
     }
 }
