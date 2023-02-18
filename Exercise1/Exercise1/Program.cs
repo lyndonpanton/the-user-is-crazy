33b54efb2bfd22b77f3652c6681041ed83de6f59
Lyndon Panton <LyndonPanton@gmail.com>
2023-02-18 22:56:11 +0000

    Completed problem 1
    Received user input with no validation

--- Exercise1/Exercise1/Program.cs
+++ Exercise1/Exercise1/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            int input = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
